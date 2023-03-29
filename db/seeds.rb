2edab7079ee1e86f032c13b2f00a731417f6788a
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-29 13:39:36 -0500

    add product.save and fix product = (lowercase) for new product

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-products = Product.new(name: "painting", price: "299", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFQacheWOXAsfg_L4Dx2eMNb06P2hvVamXtOZjs363ZakM9XJ6Lzzr4zaa5cf-wj4lUrk&usqp=CAU", description: "a portrait of a lotus flower")
+product = Product.new(name: "painting", price: "299", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFQacheWOXAsfg_L4Dx2eMNb06P2hvVamXtOZjs363ZakM9XJ6Lzzr4zaa5cf-wj4lUrk&usqp=CAU", description: "a portrait of a lotus flower")
+
+product.save
+
 
 # The data can then be loaded with the bin/rails db:seed 
