bc4825ff4bf09a24ab0885f4191ea52a0a399dc3
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-29 13:30:52 -0500

    added one product in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+products = Product.new(name: "painting", price: "299", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFQacheWOXAsfg_L4Dx2eMNb06P2hvVamXtOZjs363ZakM9XJ6Lzzr4zaa5cf-wj4lUrk&usqp=CAU", description: "a portrait of a lotus flower")
+
+# The data can then be loaded with the bin/rails db:seed 
