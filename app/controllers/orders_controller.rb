d1b767bf8fb132ced0cce51518e15dabc017d34f
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-18 14:28:48 -0500

    add authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,30 @@
 class OrdersController < ApplicationController
 
   def index 
-    @orders = Order.all 
-    render :index
+    if current_user
+      @orders = current_user.orders
+    # @orders = Order.all 
+      render :index
   end 
 
-  def show 
-    @order = Order.find_by(id: params[:id])
-    render :show
-  end 
+    def show 
+      @order = Order.find_by(id: params[:id])
+      render :show
+    end 
 
-  def create
-    @order = Order.create(
-    # user_id: params[:user_id], 
-    user_id: current_user.id,
-    product_id: params[:product_id], 
-    quantity: params[:quantity], 
-    subtotal: params[:subtotal], 
-    tax: params[:tax], 
-    total: params[:total], 
+    def create
+      @order = Order.create(
+      # user_id: params[:user_id], 
+      user_id: current_user.id,
+      product_id: params[:product_id], 
+      quantity: params[:quantity], 
+      subtotal: params[:subtotal], 
+      tax: params[:tax], 
+      total: params[:total], 
 
-    )
-    render :show 
-  end 
-end
+      )
+      render :show 
+    end 
+end 
+      end 
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete "/products/:id" => "products#destroy"
 
   post "/users" => "users#create"
+  
   post "/sessions" => "sessions#create"
 
   get "/orders" => "orders#index"
